27eb8f2141c6c71c8b075ce76975cb9dc246e200
Andre Helberg <helberg.andre@gmail.com>
2015-03-12 19:44:22 +0200

    Added basic ApplicationAdapter

--- app/app.js
+++ app/app.js
@@ -2,13 +2,19 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import DS from 'ember-data';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  ApplicationAdapter: DS.Adapter.extend({
+    find: function (store, type, id){
+      return {content:"ho",id: 1};
+    }
+  })
 });
 
 loadInitializers(App, config.modulePrefix);
