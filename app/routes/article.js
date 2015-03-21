3034a359d8478c55c40c02ba562ad10310d41c98
Andre Helberg <helberg.andre@gmail.com>
2015-03-21 10:03:21 +0200

    Can fetch and display article names from the s3 API

--- app/routes/article.js
+++ app/routes/article.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend( {
   model: function(params) {
     return this.store.find('article', 1);
+    var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
+
+    var params = {
+      Bucket: 'am-testblog', /* required */
+    };
+    
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      s3.listObjects(params, function(err, data) {
+        if (err) reject(err); // an error occurred
+        console.log(data);
+        resolve(data);  // successful response
+      });
+    });
+
   }
 });
