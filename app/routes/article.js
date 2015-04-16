7c9264e88179ffbba7e1c4a6b12d09fc2dbe26b1
Alecia Bester <alecia.bester@gmail.com>
2015-04-16 13:52:55 +0200

    feat: Refetch model on article route

--- app/routes/article.js
+++ app/routes/article.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend( {
   model: function(params) {
-    return this.store.find('article', params.key);
-    return this.store.find('article', params['article_id']);
+    return this.store.find('article', params.article_id);
+  },
+
+  setupController: function(controller, model) {
+    model.reload();
+    controller.set('model', model);
   }
 });
