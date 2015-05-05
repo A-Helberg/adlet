9262b2793f1a1e49feb6025fbe686c7ac947ed25
Andre-Marthin Helberg <helberg.andre@gmail.com>
2015-05-05 15:52:33 +0200

    feat: Create record adapter override

--- app/adapters/article.js
+++ app/adapters/article.js
@@ -37,7 +37,25 @@ export default DS.Adapter.extend({
           resolve( data );  // successful response
         });
       });
+    },
 
+    createRecord: function(store, type, snapshot) {
+      AWS.config.update({accessKeyId: '', secretAccessKey: ''});
+      var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
+
+      var params = this.params();
+      params['Key']  = snapshot.get('Key');
+      params['Body'] = snapshot.get('Body');
+
+
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        s3.putObject(params, function(err, data) {
+          if (err) {
+            reject(err); // an error occurred
+          }
+          resolve( data );  // successful response
+        });
+      });
     },
 
     find: function (store, type, id){
