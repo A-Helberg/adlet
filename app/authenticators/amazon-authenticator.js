4104b59985d95676909a707cdc0963427f343814
Alecia Bester <alecia.bester@gmail.com>
2015-05-18 17:02:47 +0200

    refactor: authenticator with a refresh credentials promise

--- app/authenticators/amazon-authenticator.js
+++ app/authenticators/amazon-authenticator.js
@@ -3,36 +3,26 @@ import Ember from 'ember';
 
 export default Base.extend({
   restore: function(options) {
-    var credentials = new AWS.Credentials(options);
-    return new Ember.RSVP.Promise(function(resolve, reject){
-      credentials.refresh(function(err) {
-        if (err) {
-          console.log("!!!! Error restoring session: ", err);
-          reject(err); // an error occurred
-        }
-        AWS.config.update({credentials: credentials});
-        resolve( options );  // successful response
-      });
-    });
+    return this.refreshCredentials(options);
   },
   authenticate: function(options) {
-    // AWS.config.update({accessKeyId: options.accessKeyId, secretAccessKey: options.secretAccessKey});
-    // var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
-    // console.log('Authenticate?', options);
-
-    var credentials = new AWS.Credentials(options);
-
-    return new Ember.RSVP.Promise(function(resolve, reject){
-      credentials.refresh(function(err) {
-        if (err) {
-          reject(err); // an error occurred
-        }
-        AWS.config.update({credentials: credentials});
-        resolve( options );  // successful response
-      });
-    });
+    return this.refreshCredentials(options);
   },
   invalidate: function(data) {
     console.log('Not called :(');
+  },
+
+  refreshCredentials: function(credentialObject){
+    var credentials = new AWS.Credentials(credentialObject);
+
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      credentials.refresh(function(err) {
+        if (err) {
+          reject(err);
+        }
+        AWS.config.update({credentials: credentials});
+        resolve(credentialObject);
+      });
+    });
   }
 });
