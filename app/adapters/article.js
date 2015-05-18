ece9c231c24e44f876aadd5d3d9447e889fb8f4f
Alecia Bester <alecia.bester@gmail.com>
2015-05-18 17:01:29 +0200

    refactor: adapter promises

--- app/adapters/article.js
+++ app/adapters/article.js
@@ -6,95 +6,59 @@ export default DS.Adapter.extend({
         Bucket: 'am-testblog', /* required */
       };
     },
-    list: function(){
-      debugger
+
+    apiPromise: function(awsFunction, params){
       var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
 
-
-      var params = this.params();
-
       return new Ember.RSVP.Promise(function(resolve, reject){
-        s3.listObjects(params, function(err, data) {
-          if (err) {
-            reject(err);
-          } // an error occurred
-          resolve( data );  // successful response
-        });
-      });
-    },
-
-    getArticle: function(key){
-      var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
-
-      var params = this.params();
-      params['Key'] = key;
-
-      return new Ember.RSVP.Promise(function(resolve, reject){
-        s3.getObject(params, function(err, data) {
-          if (err) {
-            reject(err); // an error occurred
-          }
-          resolve( data );  // successful response
-        });
+        var apiFunction = s3[awsFunction];
+        if (apiFunction){
+          apiFunction = apiFunction.bind(s3);
+          apiFunction(params, function(err, data) {
+            if (err) {
+              reject(err);
+            }
+            resolve(data);
+          });
+        } else {
+          reject("Function is not a valid AWS S3 API method.");
+        }
       });
     },
 
     createRecord: function(store, type, snapshot) {
-      var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
-
       var params = this.params();
       params['Key']  = snapshot.get('Key');
       params['Body'] = snapshot.get('Body');
 
-      return new Ember.RSVP.Promise(function(resolve, reject) {
-        s3.putObject(params, function(err, data) {
-          if (err) {
-            reject(err); // an error occurred
-          }
-          resolve( data );  // successful response
-        });
-      });
+      return this.apiPromise('putObject', params);
     },
 
     updateRecord: function(store, type, snapshot) {
-      var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
-
       var params = this.params();
       params['Key']  = snapshot.get('Key');
       params['Body'] = snapshot.get('Body');
 
-      return new Ember.RSVP.Promise(function(resolve, reject) {
-        s3.putObject(params, function(err, data) {
-          if (err) {
-            reject(err); // an error occurred
-          }
-          resolve( data );  // successful response
-        });
-      });
+      return this.apiPromise('putObject', params);
     },
 
     deleteRecord: function(store, type, snapshot) {
-      
-      var s3 = new AWS.S3({region: 'us-west-2', maxRetries: 5});
-
       var params = this.params();
       params['Key']  = snapshot.get('Key');
 
-      return new Ember.RSVP.Promise(function(resolve, reject) {
-        s3.deleteObject(params, function(err, data) {
-          if (err) {
-            reject(err); // an error occurred
-          }
-          resolve( data );  // successful response
-        });
-      });
+      return this.apiPromise('deleteObject', params);
     },
 
     find: function (store, type, id){
-      return this.getArticle(id);
+      var params = this.params();
+      params['Key'] = id;
+
+      return this.apiPromise('getObject', params);
     },
 
     findAll: function(){
-      return this.list();
+      var params = this.params();
+
+      return this.apiPromise('listObjects', params);
     }
   });
