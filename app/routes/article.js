bec981fe125156d120c5fd46cd3501e0caa1be2e
Alecia Bester <alecia.bester@gmail.com>
2015-05-19 13:51:22 +0200

    feat: Authenticate on all existing routes

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend( {
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params) {
     return this.store.find('article', params.article_id);
   },
