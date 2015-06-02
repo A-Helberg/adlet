6045216adced7003f8304138e7e310fab5a62129
Andre-Marthin Helberg <helberg.andre@gmail.com>
2015-06-02 15:00:53 +0200

    fix: convert body to string from binary array

--- app/serializers/article.js
+++ app/serializers/article.js
@@ -1,14 +1,24 @@
 import DS from "ember-data";
 
+function bin2String(array) {
+  var result = "";
+  for (var i = 0; i < array.length; i++) {
+    result += String.fromCharCode(array[i]);
+  }
+  return result;
+}
+
 export default DS.Serializer.extend({
   extract: function(store, type, payload, id, requestType) {
     if (requestType === "findAll") {
       payload = payload['Contents'];
       payload.forEach(function(element, index, array) {
         element['id'] = element['Key'];
+        element['Body'] = bin2String(element['Body']);
       });
     } else if (requestType === "find") {
       payload['Key'] = id;
+      payload['Body'] = bin2String(payload['Body']);
     }
 
     if (requestType !== 'findAll') {
