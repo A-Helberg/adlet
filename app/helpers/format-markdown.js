276152e43d78d082ee78008d3b5f675e2e43c7d6
Andre-Marthin Helberg <helberg.andre@gmail.com>
2015-06-02 14:56:57 +0200

    fix: format-markdown helper

--- app/helpers/format-markdown.js
+++ app/helpers/format-markdown.js
@@ -1,15 +1,6 @@
 import Ember from 'ember';
-import layout from '../templates/components/format-markdown';
 
 var showdown = new Showdown.converter();
-
-function bin2String(array) {
-  var result = "";
-  for (var i = 0; i < array.length; i++) {
-    result += String.fromCharCode(array[i]);
-  }
-  return result;
-}
 
 var formatMarkdown = Ember.HTMLBars.makeBoundHelper(function (arr) {
     if (!arr || !arr.length) {
@@ -17,7 +8,7 @@ var formatMarkdown = Ember.HTMLBars.makeBoundHelper(function (arr) {
     }
 
     var escapedhtml = '',
-        markdown = bin2String(arr[0]) || '';
+        markdown = arr[0] || '';
 
     // convert markdown to HTML
 
