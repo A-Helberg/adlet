d8c6715d0b0db8a0f453b478a826acf1f266e21e
Andre-Marthin Helberg <helberg.andre@gmail.com>
2015-06-03 12:07:12 +0200

    feat: remove uncommitted model

--- app/routes/articles/new.js
+++ app/routes/articles/new.js
@@ -4,5 +4,9 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
     return this.store.createRecord('article');
-  }
+  },
+
+  deactivate: function() {
+    this.get('controller.content').rollback();
+  }
 });
