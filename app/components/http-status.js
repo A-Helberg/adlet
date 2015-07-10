eac90d25c2f5088323d94a04c60530210dad7035
Alecia Bester <alecia.bester@gmail.com>
2015-07-10 15:45:31 +0200

    feat: Added http status component

---
+++ app/components/http-status.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import layout from '../templates/components/http-status';
+import httpStatusName from 'npm:http-status-name';
+import stringHumanize from 'npm:string-humanize';
+
+export default Ember.Component.extend({
+  layout: layout,
+
+  getStatusName: function() {
+    this.set('statusName', stringHumanize(httpStatusName(this.get('status'))));
+  }.on('init')
+});
