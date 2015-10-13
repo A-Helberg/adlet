a27e486f6e5749afd7affd4533ce7af625e0ce6a
Andre <helberg.andre@gmail.com>
2015-10-13 19:00:20 +0200

    chore: added ember-simple-auth-testing

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -3,6 +3,9 @@ import Application from '../../app';
 import Router from '../../router';
 import config from '../../config/environment';
 
+import initializeTestHelpers from 'simple-auth-testing/test-helpers';
+initializeTestHelpers();
+
 export default function startApp(attrs) {
   var application;
 
