da7d3550abe9f9b1f1bca8c45eefac52e1a88f25
Alecia Bester <alecia.bester@gmail.com>
2015-10-26 21:29:48 +0200

    refactor: moved code around :p

--- app/controllers/login/index.js
+++ app/controllers/login/index.js
@@ -4,12 +4,12 @@ export default Ember.Controller.extend({
   authenticator: 'authenticator:amazon-authenticator',
   actions: {
     login: function() {
-      var _this = this;
-      var credentials = this.getProperties('accessKeyId', 'secretAccessKey');
-      this.get('session').authenticate('authenticator:amazon-authenticator', credentials).then( function() {
-          _this.transitionToRoute('admin.articles');
-        });
-    }
+      var _this   = this,
+      credentials = this.getProperties('accessKeyId', 'secretAccessKey');
 
+      this.get('session').authenticate('authenticator:amazon-authenticator', credentials).then( function() {
+        _this.transitionToRoute('admin.articles');
+     });
+    }
   }
 });
