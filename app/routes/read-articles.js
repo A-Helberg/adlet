ce3e037c36d60a9f343802c0f54993aa38643b51
Alecia Bester <alecia.bester@gmail.com>
2015-11-10 08:08:42 +0200

    chore: remove * the files

--- app/routes/read-articles.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import ReadOnlyMixin from 'adlet/mixins/read-only-route';
-
-export default Ember.Route.extend(ReadOnlyMixin, {
-  model: function(){
-    return this.store.findAll('article');
-  }
-});
