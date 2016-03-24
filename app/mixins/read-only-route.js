752ff22a339241b2c1c033d4e9bd9953be1ac6ce
Andre <helberg.andre@gmail.com>
2016-03-24 09:20:02 +0200

    fix jshint warnings in read-only-route mixin

--- app/mixins/read-only-route.js
+++ app/mixins/read-only-route.js
@@ -5,7 +5,7 @@ import ENV from 'adlet/config/environment';
 export default Ember.Mixin.create({
   beforeModel: function() {
     let credentials = AWS.config.credentials;
-    if (credentials == null || (credentials.accessKeyId == '' || credentials.secretAccessKey == '')){
+    if (credentials == null || (credentials.accessKeyId === '' || credentials.secretAccessKey === '')){
         AWS.config.update({'accessKeyId': ENV.ReadOnlyAccessKeyID , 'secretAccessKey': ENV.ReadOnlySecretAccessKey});
     }
     return this._super();
