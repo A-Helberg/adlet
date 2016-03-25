bea2fa1bf921898935182d95dcbf637f4e4f216f
Andre <helberg.andre@gmail.com>
2016-03-25 09:32:55 +0200

    remove read-only-route mixin

--- app/routes/read-articles.js
+++ app/routes/read-articles.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import ReadOnlyMixin from 'adlet/mixins/read-only-route';
 
-export default Ember.Route.extend(ReadOnlyMixin, {
+export default Ember.Route.extend({
   model: function(){
     return this.store.findAll('article');
   }
