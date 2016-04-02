561927db27b4f3486c1dd9a3f79a5840fc9011a7
Andre <helberg.andre@gmail.com>
2016-04-02 09:29:21 +0200

    fix new admin article route

--- app/routes/admin/articles/edit.js
+++ app/routes/admin/articles/edit.js
@@ -2,11 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function() {
-    return this.store.createRecord('article');
+  model: function(params) {
+    return this.store.find('article', params.article_id);
   }
-
-  //deactivate: function() {
-  //  this.get('controller.content').rollback();
-  //}
 });
