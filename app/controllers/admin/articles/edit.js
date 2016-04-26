16c8c7b5cd0b2ba625d380a4ac00ed12725054b0
Andre Helberg <helberg.andre@gmail.com>
2016-04-26 23:45:38 +0200

    mark article being edited in admin-articles template

--- app/controllers/admin/articles/edit.js
+++ app/controllers/admin/articles/edit.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  articlesController: Ember.inject.controller('admin.articles'),
+
+  modelChanged: Ember.observer('model', function() {
+    this.get('articlesController').send('markArticleAsEditing', this.get('model'));
+  }),
+
   actions: {
     save() {
       this.get('model').save().then(() => {
