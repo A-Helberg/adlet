b5487537c7b114c98d0d7115713793ad48373066
Andre Helberg <helberg.andre@gmail.com>
2016-05-15 10:19:34 +0200

    article model can have permissions and get objects url

--- app/models/article.js
+++ app/models/article.js
@@ -2,11 +2,14 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 var Article = DS.Model.extend({
+  s3: Ember.inject.service(),
   // The AWS Key for this Article
   key: DS.attr('string'),
   // These are private attributes used to support async fetching of an article's body
   _body: DS.attr('string'),
   _bodyHasBeenFetched: DS.attr('boolean'),
+  // If not null, the permission will be set when saving the model
+  _permission: null,
 
   body: Ember.computed('_body', {
     get () {
@@ -17,6 +20,12 @@ var Article = DS.Model.extend({
     },
     set(key, value) {
       return this.set('_body', value);
+    }
+  }),
+
+  url: Ember.computed({
+    get() {
+      return this.get('s3').getUrl(this.get('key'));
     }
   })
 });
